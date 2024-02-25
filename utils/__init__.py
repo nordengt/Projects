27a8a67d74a726e959683d6666850f1afc72e41b
nordengt <nordenghisingt123@gmail.com>
2024-02-25 19:32:35 +0545

    adding metrics + fixes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-from .plot import plot_3x3
+from .plot import plot_3x3, plot_loss_accuracy
+from .metrics import calculate_accuracy
 
-__all__ = ["plot_3x3"]
+__all__ = ["plot_3x3", "plot_loss_accuracy", "calculate_accuracy"]
--- utils/plot.py
+++ utils/plot.py
@@ -1,28 +1,14 @@
-import torch
+import os
 import matplotlib.pyplot as plt
 from typing import List
-
-def plot_3x3(
-        dataloader: torch.utils.data.DataLoader, 
-        classes: List[str] = None
-    ) -> None:
-    images, labels = next(iter(dataloader))
-    
-    _, axes = plt.subplots(3, 3, figsize=(4, 4))
-    
-    for i, ax in enumerate(axes.flat):
-        ax.imshow(images[i].numpy().transpose((1, 2, 0)))
-        ax.set_title(classes[labels[i].item()])
-        ax.axis("off")
-    
-    plt.tight_layout()
-    plt.show()
 
 def plot_loss_accuracy(
         train_losses: List[int], 
         test_losses: List[int], 
         train_accuracies: List[int], 
-        test_accuracies: List[int],
+        test_accuracies: List[int], 
+        save: bool = False,
+        name: str = ""
     ) -> None:
     epochs = range(1, len(train_losses) + 1)
 
@@ -45,4 +31,9 @@ def plot_loss_accuracy(
     plt.legend()
 
     plt.tight_layout()
+    
+    if save:
+        if not os.path.exists("results"): os.makedirs("results")
+        plt.savefig(f"results/{name}.png")
+    
     plt.show()
